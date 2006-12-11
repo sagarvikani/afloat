c2648dc1a1ca3c232a87b3930a8383a09059abe3
millenomi <millenomi@b1708594-7f1a-0410-9c04-c79b9fbae0fb>
2006-12-11 22:19:34 +0000

    All functionality for b4 in. This thing works! (Added proper authorization for i386 and login item addition/removal.)

--- trunk/AfloatAgent/PrefPane/AfloatPrefPane.m
+++ trunk/AfloatAgent/PrefPane/AfloatPrefPane.m
@@ -10,13 +10,15 @@
  *  Please see the included LICENSE file for details.
  */
 
-
-#import "../AfloatAgentCommunication.h"
-
 #import "AfloatPrefPane.h"
+
+#import "LoginItemsAE.h"
+
 #import <unistd.h>
 #import <signal.h>
 #import <sys/types.h>
+
+#import <Security/Security.h>
 
 #define kAfloatAgentBundleIdentifier @"net.infinite-labs.Afloat.Agent"
 
@@ -47,14 +49,131 @@
 	return x;
 }
 
+- (BOOL) requiresAuthorization {
+	//#define kAfloatDebugAuthorization 1
+	// please note: Afloat is meant to be built as a 32-bit binary thingy, hence the i386.
+	// who knows what tricks can x86_64 play with mach_*?
+	// I don't want to be the one who finds out.
+#if defined(__i386__) || defined(kAfloatDebugAuthorization)
+	
+	NSString* pathToAgentBundle = [[NSBundle bundleForClass:[self class]] pathForResource:@"Afloat Agent" ofType:@"app"];
+	NSBundle* agentBundle = [[[NSBundle alloc] initWithPath:pathToAgentBundle] autorelease];
+	NSDictionary* stats = [[NSFileManager defaultManager] fileAttributesAtPath:[agentBundle executablePath] traverseLink:NO];
+	
+	if ([[stats objectForKey:NSFileGroupOwnerAccountID] longValue] == 9 &&
+		[[stats objectForKey:NSFilePosixPermissions] longValue] == 02755)
+		return NO;
+	
+	long minor, build;
+	if (Gestalt(gestaltSystemVersionMinor, &minor) != noErr) {
+		// ABORT! Todo.
+		return NO;
+	}
+	
+	if (Gestalt(gestaltSystemVersionBugFix, &build) != noErr) {
+		// ABORT! Todo.
+		return NO;
+	}
+	
+	return (minor >= 4 && build >= 6); // i386 10.4.6 and later.
+#else
+	return NO; // no need to check this on PPC
+#endif
+}
+
+- (BOOL) authorize {
+	OSStatus err;
+	AuthorizationRef auth;
+	
+	if ((err = AuthorizationCreate(NULL, kAuthorizationEmptyEnvironment, kAuthorizationFlagDefaults, &auth)) == noErr) {
+		NSLog(@"Afloat preference pane: Authorizing the Agent.");
+		
+		NSString* pathToAgentBundle = [[NSBundle bundleForClass:[self class]] pathForResource:@"Afloat Agent" ofType:@"app"];
+		NSBundle* agentBundle = [[[NSBundle alloc] initWithPath:pathToAgentBundle] autorelease];
+		const char* pathToAgentExecC = [[NSFileManager defaultManager] fileSystemRepresentationWithPath:[agentBundle executablePath]];
+
+		char* args[] = {
+			"--Afloat-RepairPrivileges",
+			NULL
+		};
+		
+		AuthorizationExecuteWithPrivileges(auth, pathToAgentExecC, kAuthorizationFlagDefaults, args, NULL);
+		
+		AuthorizationFree(auth, kAuthorizationFlagDestroyRights);
+		
+		usleep(500000);
+	} else
+		return NO;
+	
+	return ![self requiresAuthorization];
+}
+
+- (BOOL) canProceedWithEnablingWithUIAllowed:(BOOL) canAsk {
+	if (![self requiresAuthorization])
+		return YES;
+	
+	// set up authorization asynchronously if canAsk
+	if (canAsk) {
+		NSAlert* alert = [[NSAlert new] autorelease];
+		
+		[alert setMessageText:
+			NSLocalizedString(@"Afloat must be authorized by an administrator before it can be enabled.",
+							  @"Authorization prompt message.")
+			];
+		
+		[alert setInformativeText:
+			NSLocalizedString(@"For security reasons, recent Mac OS X versions require an administrator to authorize modules, such as Afloat, that change the way other applications work.\n\nThis must be done only once, and will require an administrator's name and password.",
+							  @"Authorization prompt info text")
+			];
+		
+		[alert addButtonWithTitle:
+			NSLocalizedString(@"Authorize", @"Authorization prompt accept button")
+			];
+		[alert addButtonWithTitle:
+			NSLocalizedString(@"Cancel", @"Authorization prompt cancel button")
+			];
+		
+		[alert beginSheetModalForWindow:[[self mainView] window] modalDelegate:self didEndSelector:@selector(authorizationPromptDidEnd:returnCode:nothing:) contextInfo:nil];
+	}
+	
+	return NO;
+}
+
+- (void) authorizationPromptDidEnd:(NSAlert*) alert returnCode:(int) retCode nothing:(void*) nothing {
+	if (retCode == NSAlertFirstButtonReturn) {
+		if ([self authorize]) [self setAfloatEnabled:YES];
+	}
+}
+
 - (void) setAfloatEnabled:(BOOL) enabled {
+	[self willChangeValueForKey:@"afloatEnabled"];
+	NSString* pathToAgentBundle = [[NSBundle bundleForClass:[self class]] pathForResource:@"Afloat Agent" ofType:@"app"];
+	NSURL* URLToAgentBundle = [NSURL fileURLWithPath:pathToAgentBundle];
+	
 	if (enabled == NO) {
-		if (![self afloatEnabled]) return;
+		if (![self afloatEnabled]) goto AfloatEnabledCleanup;
 		
 		[[self afloatAgent] disable];
 	
-		sleep(1); // we give it time to die gracefully
-
+		// remove the login item
+		NSString* pathToAgentBundle = [[NSBundle bundleForClass:[self class]] pathForResource:@"Afloat Agent" ofType:@"app"];
+		NSURL* URLToAgentBundle = [NSURL fileURLWithPath:pathToAgentBundle];
+		
+		NSArray* loginItems = nil;
+		LIAECopyLoginItems((CFArrayRef*) &loginItems);
+		[loginItems autorelease];
+		
+		int i, j;
+		for (i = 0, j = 0; i < [loginItems count]; i++, j++) {
+			// no nsenumerators, because we're going to need the index
+			if ([[[loginItems objectAtIndex:i] objectForKey:(id) kLIAEURL] isEqualTo:URLToAgentBundle]) {
+				LIAERemove((CFIndex)j);
+				j--;
+			}
+		}
+		
+		sleep(1); // we give the agent time to die gracefully
+		
 		pid_t agentPID = [self processIDForAfloatAgent];
 		if (agentPID != 0) {
 			kill(agentPID, SIGKILL);
@@ -69,11 +188,18 @@
 			[agentWasKilled beginSheetModalForWindow:[[self mainView] window] modalDelegate:nil didEndSelector:NULL contextInfo:NULL];
 		}
 	} else {
-		if ([self afloatEnabled]) return;
+		if ([self afloatEnabled]) goto AfloatEnabledCleanup;
+		if (![self canProceedWithEnablingWithUIAllowed:YES]) goto AfloatEnabledCleanup;
+		
 		NSString* pathToAgent = [[NSBundle bundleForClass:[self class]] pathForResource:@"Afloat Agent" ofType:@"app"];
-		[[NSWorkspace sharedWorkspace] launchApplication:pathToAgent];
+		[[NSWorkspace sharedWorkspace] openFile:pathToAgent withApplication:nil andDeactivate:NO];
+		
+		LIAEAddURLAtEnd((CFURLRef) URLToAgentBundle, false);
 		sleep(1); // to give it time to start
 	}
+	
+AfloatEnabledCleanup:
+		[self performSelector:@selector(didChangeValueForKey:) withObject:@"afloatEnabled" afterDelay:0.1];
 }
 
 @end
