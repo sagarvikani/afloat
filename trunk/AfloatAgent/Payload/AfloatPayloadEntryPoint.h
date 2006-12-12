d840ed7adee40bc83cd90660ca8c462933a3346b
millenomi <millenomi@b1708594-7f1a-0410-9c04-c79b9fbae0fb>
2006-12-12 17:01:34 +0000

    Icons added. Everything is now under the LGPL. We're getting ready to launch.

--- trunk/AfloatAgent/Payload/AfloatPayloadEntryPoint.h
+++ trunk/AfloatAgent/Payload/AfloatPayloadEntryPoint.h
@@ -4,13 +4,18 @@
 //
 
 /*
- *  This file is part of Afloat and is © Emanuele Vulcano, 2006.
- *  <afloat@infinite-labs.net>
- *  
- *  Afloat's source code is licensed under a BSD license.
- *  Please see the included LICENSE file for details.
- */
 
+Copyright © 2006, Emanuele Vulcano.
+
+This file is part of Afloat.
+
+    Afloat is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+
+    Afloat is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License along with Afloat; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+
+*/
 
 // I always wanted to write a ((constructor)) thingie, and
 // here I am doing it for a useful purpose :)
--- trunk/AfloatAgent/Payload/AfloatWindowAlphaAnimation.m
+++ trunk/AfloatAgent/Payload/AfloatWindowAlphaAnimation.m
@@ -1,10 +1,20 @@
 //
 //  AfloatWindowAlphaAnimation.m
 //  AfloatAgent
-//
-//  Created by ∞ on 13/11/06.
-//  Copyright 2006 __MyCompanyName__. All rights reserved.
-//
+
+/*
+
+Copyright © 2006, Emanuele Vulcano.
+
+This file is part of Afloat.
+
+    Afloat is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+
+    Afloat is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License along with Afloat; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+
+*/
 
 #import "AfloatWindowAlphaAnimation.h"
 
